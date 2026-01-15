dd9ea601c45349f3e06b3bb36459fdd27332647c
cros <cros@nextads.es>
2026-01-15 16:38:11 +0100

    [FIX] corregir error del .items()

--- no_send_mail_assign_invoice/models/account_move.py
+++ no_send_mail_assign_invoice/models/account_move.py
@@ -6,17 +6,15 @@ from odoo import _, api, exceptions, fields, models, tools, registry, SUPERUSER_
 class AccountMove(models.AbstractModel):
     _inherit = 'account.move'
 
-    def _notify_get_recipients(self, message, recipients_data):
+    def _notify_get_recipients(self, message, msg_vals, **kwargs):
         """
         Odoo 18 hook:
         Evita emails cuando se asigna user_id en facturas.
         """
-        recipients_data = super()._notify_get_recipients(message, recipients_data)
+        recipients_data = super(AccountMove, self)._notify_get_recipients(message, msg_vals, **kwargs)
 
         if message.model == "account.move" and message.subtype_id and \
            message.subtype_id.internal:
-            for partner_id, data in recipients_data.items():
-                # Forzar notificación interna (inbox)
-                data["notification_type"] = "inbox"
+            return {}
 
         return recipients_data
